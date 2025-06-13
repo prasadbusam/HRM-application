35c11a5a9902711d03f0a40b674540f7bb8ae583
Prasad Busam <121351552+prasadbusam@users.noreply.github.com>
2025-06-13 16:26:59 +0530

    Updated Admin and step definitions, removed ConfigReader, added WaitHelper

--- src/main/java/pageObjects/Admin.java
+++ src/main/java/pageObjects/Admin.java
@@ -1,15 +1,89 @@
 package pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import utilities.WaitHelper;
 
 public class Admin {
 	WebDriver driver;
+	WaitHelper waithelper;
 
 	public Admin(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
+		waithelper = new WaitHelper(driver);
 	}
+	//(//*[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[1]
+	@FindBy(xpath="(//*[@class='oxd-text oxd-text--span oxd-main-menu-item--name'])[1]") WebElement Admin;
+	//(//*[@type='button'])[6]
+	@FindBy(xpath="//*[@type='button'])[6]") WebElement Addbutton;
+	@FindBy(xpath="(//*[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[1]") WebElement select;
+	@FindBy(xpath="(//*[text()='Admin'])[3]") WebElement admin;
+	@FindBy(xpath="//*[@placeholder='Type for hints...']") WebElement empname;
+	//ctrl+shift+p emulate a focused page
+	@FindBy(xpath="(//*[@class='oxd-icon bi-caret-down-fill oxd-select-text--arrow'])[2]") WebElement status;
+	@FindBy(xpath="//*[text()='Enabled']") WebElement enabled;
+	@FindBy(xpath="//*[@class='oxd-input oxd-input--active oxd-input--error']") WebElement username;
+	@FindBy(xpath="(//*[@type='password'])[1]") WebElement password;
+	@FindBy(xpath="(//*[@type='password'])[2]") WebElement cnfpassword;
+	@FindBy(xpath="//*[@type='submit']") WebElement save;
 	
 	
+	public void clickAdminmodule() {
+		waithelper.waitForElement(Admin, 20);
+		Admin.click();
+	}
+	public void clickAddbtton() {
+		waithelper.waitForElement(Addbutton, 20);
+		Addbutton.click();
+	}
+	  // Method 1: Click the dropdown
+    public void clickDropdownArrow() {
+    	waithelper.waitForElement(select, 20);
+        select.click();
+    }
+
+    // Method 2: Click the 'Admin' option
+    public void clickAdminOption() {
+    	waithelper.waitForElement(admin, 20);
+        admin.click();
+    }
+    
+    public void enterEmployeeName(String empName) {
+    	waithelper.waitForElement(empname, 20);
+        empname.sendKeys(empName);
+    }
+
+    public void clickStatusDropdown() {
+    	waithelper.waitForElement(status, 20);
+        status.click();
+    }
+
+    public void selectEnabledStatus() {
+    	waithelper.waitForElement(enabled, 20);
+        enabled.click();
+    }
+
+    public void enterUsername(String user) {
+    	waithelper.waitForElement(username, 20);
+        username.sendKeys(user);
+    }
+
+    public void enterPassword(String pass) {
+    	waithelper.waitForElement(password, 20);
+        password.sendKeys(pass);
+    }
+
+    public void enterConfirmPassword(String confirmPass) {
+    	waithelper.waitForElement(cnfpassword, 20);
+        cnfpassword.sendKeys(confirmPass);
+    }
+
+    public void clickSave() {
+    	waithelper.waitForElement(save, 20);
+        save.click();
+    }
 }
--- src/test/java/stepDefinitions/AdminHRMsteps.java
+++ src/test/java/stepDefinitions/AdminHRMsteps.java
@@ -2,33 +2,46 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import pageObjects.Admin;
+import pageObjects.Login;
+import pageObjects.Pim;
 
 public class AdminHRMsteps extends Base_class1 {
+	
+	Admin admin;
+	Pim pim;
+    Login login;
 	
 	@Given("I launchess the application")
 	public void i_launchess_the_application() {
 		
+	        launchBrowser(); // Only launches once
+	        login = new Login(driver);
+	        pim = new Pim(driver);
+	        admin =new Admin(driver);
+	        }
 	    
-	}
 
 	@Given("I logged with username {string} and password {string}")
-	public void i_logged_with_username_and_password(String string, String string2) {
-	    
+	public void i_logged_with_username_and_password(String username, String password) {
+		login.Setusername(username);
+        login.Setuserpassword(password);
+        login.clickloginbtn();
 	}
 
 	@Given("I navigate to ADMIN page")
 	public void i_navigate_to_admin_page() {
-	    
+	    admin.clickAdminmodule();
 	}
 
 	@When("click on the add button")
 	public void click_on_the_add_button() {
-	    
+	    admin.clickAddbtton();
 	}
 
 	@When("click on the user role dropdown")
 	public void click_on_the_user_role_dropdown() {
-	    
+	    admin.clickDropdownArrow();
 	}
 
 	@When("select the Admin user option")
